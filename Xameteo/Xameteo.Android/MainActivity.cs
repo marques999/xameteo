09b02faef3819e24af1d9bbc772c08ae7e9222a0
Diogo Marques <marques999@gmail.com>
2017-11-27 16:32:40 +0000

    Color scheme

--- Xameteo/Xameteo.Android/MainActivity.cs
+++ Xameteo/Xameteo.Android/MainActivity.cs
@@ -1,33 +1,29 @@
-﻿using System;
-
+﻿using Android.OS;
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace Xameteo.Droid
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [Activity(Label = "Xameteo.Android", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : FormsAppCompatActivity
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bundle"></param>
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(bundle);
-
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-
+            Forms.Init(this, bundle);
             LoadApplication(new App());
-        }
-
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
         }
     }
 }
